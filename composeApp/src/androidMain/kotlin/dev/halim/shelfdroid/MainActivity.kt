8b0e0df2d0bef7fbf131c5b8c104d9205f2a8d61
Fernando <100nandoo@gmail.com>
2024-10-22 12:50:13 +0800

    add preview for LoginScreen

--- composeApp/src/androidMain/kotlin/dev/halim/shelfdroid/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/dev/halim/shelfdroid/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import dev.halim.shelfdroid.login.LoginScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,4 +21,10 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun AppAndroidPreview() {
     App()
+}
+
+@Preview(showSystemUi = true, showBackground = true)
+@Composable
+fun LoginScreenPreview(){
+    LoginScreen()
 }
