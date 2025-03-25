e0944e4c150e8359a1f8565e039e3abeeda2b2ed
Fernando <100nandoo@gmail.com>
2025-03-25 10:08:48 +0800

    feat(Splashscreen): add androidx core splashscreen implementation

--- app/src/main/java/dev/halim/shelfdroid/ui/MainActivity.kt
+++ app/src/main/java/dev/halim/shelfdroid/ui/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import dagger.hilt.android.AndroidEntryPoint
 import dev.halim.shelfdroid.core.data.UserPrefs
 import dev.halim.shelfdroid.core.ui.theme.ShelfDroidTheme
@@ -20,6 +21,8 @@ class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        installSplashScreen()
         setContent {
             ShelfDroidTheme(userPrefs.darkMode) {
                 Surface(
