2d3355dfefff2ad5a8d55a37ce4c09cf7e9f9524
Fernando <100nandoo@gmail.com>
2026-05-14 20:10:39 +0800

    Add scoped preview harnesses

--- core-ui/src/main/java/dev/halim/shelfdroid/core/ui/screen/home/item/HomeItem.kt
+++ core-ui/src/main/java/dev/halim/shelfdroid/core/ui/screen/home/item/HomeItem.kt
@@ -1,7 +1,13 @@
 package dev.halim.shelfdroid.core.ui.screen.home.item
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.lazy.grid.LazyGridItemScope
+import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.runtime.Composable
+import dev.halim.shelfdroid.core.ui.preview.Defaults.HOME_BOOKS
+import dev.halim.shelfdroid.core.ui.preview.Defaults.HOME_PODCASTS
+import dev.halim.shelfdroid.core.ui.preview.LazyGridItemPreviewWrapper
+import dev.halim.shelfdroid.core.ui.preview.ShelfDroidPreview
 
 @Composable
 fun LazyGridItemScope.HomeItem(
@@ -33,6 +39,52 @@ fun LazyGridItemScope.HomeItem(
       unfinishedEpisodeCount = unfinishedEpisodeCount,
       onClick = onClick,
       onLongClick = onLongClick,
+    )
+  }
+}
+
+@ShelfDroidPreview
+@Composable
+private fun HomeItemGridPreview() {
+  val book = HOME_BOOKS.first()
+
+  LazyGridItemPreviewWrapper(
+    columns = GridCells.Fixed(2),
+    animated = true,
+    reverseLayout = true,
+    verticalArrangement = Arrangement.Bottom,
+  ) {
+    HomeItem(
+      id = book.id,
+      title = book.title,
+      author = book.author,
+      cover = book.cover,
+      onClick = {},
+      onLongClick = {},
+    )
+  }
+}
+
+@ShelfDroidPreview
+@Composable
+private fun HomeItemListPreview() {
+  val podcast = HOME_PODCASTS.first()
+
+  LazyGridItemPreviewWrapper(
+    columns = GridCells.Fixed(1),
+    animated = true,
+    reverseLayout = true,
+    verticalArrangement = Arrangement.Bottom,
+  ) {
+    HomeItem(
+      listView = true,
+      id = podcast.id,
+      title = podcast.title,
+      author = podcast.author,
+      cover = podcast.cover,
+      unfinishedEpisodeCount = podcast.unfinishedCount,
+      onClick = {},
+      onLongClick = {},
     )
   }
 }
